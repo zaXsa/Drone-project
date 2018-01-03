d2349d3c31ffb761cd426f1b72e0674d9d6da9a2
zaXsa <33417900+zaXsa@users.noreply.github.com>
2018-01-03 18:28:15 +0100

    Testing

---
+++ Arduino_to_Cpp/Arduino_to_Cpp/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 3-1-2018 18:26:55
+ * Author : renzo
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
