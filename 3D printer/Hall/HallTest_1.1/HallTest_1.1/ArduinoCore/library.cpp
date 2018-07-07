5c4f99907a35962767aa08bcc3a5731e11d35b6d
zaXsa <33417900+zaXsa@users.noreply.github.com>
2018-07-07 22:35:58 +0200

    might work who knows

---
+++ 3D printer/Hall/HallTest_1.1/HallTest_1.1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 7-7-2018 21:46:44
+ * Author : renzo
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ 3D printer/Hall/HallTest_1.1/HallTest_1.1/move_test/main.cpp
@@ -0,0 +1,46 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include "StepperDriver.h" 
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+
+
+#define motor_steps 200  //定義步進馬達每圈的步數  
+#define step_divisition 16
+#define en_pin 5
+#define cw_pin 6
+#define clk_pin 7
+
+StepperDriver ss(motor_steps, step_divisition, en_pin, cw_pin, clk_pin);  
+
+int main(){
+	DDRB &= ~(1<<DDB0);				// Arduino pin 8
+	
+	PCMSK0 |= (1<<PCINT0);
+	PCICR |= (1<<PCIE0);
+
+	ss.setSpeed(160);
+	ss.powerEnable(true);
+	ss.positioning();
+	delay(1600);
+
+	sei();
+
+	while (1){
+		ss.setSpeed(160);
+		ss.step(20);
+		delay(100);
+	}
+}
+
+ISR(PCINT0_vect){
+	if ((PINB & (1 << PINB0)) == 1 ){
+		ss.powerEnable(false);
+	} else if ((PINB & (1 << PINB0)) == 0 ){
+		ss.powerEnable(true);
+	}
+}
