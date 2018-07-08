66af47d56cc89bc437637d38f201fd92b1b72581
zaXsa <33417900+zaXsa@users.noreply.github.com>
2018-07-08 13:48:16 +0200

    Fk Arduino IDE

---
+++ 3D printer/Hall/HallTest_1.2/HallTest_1.2/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 8-7-2018 13:41:59
+ * Author : renzo
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
--- 3D printer/Hall/HallTest_1.2/HallTest_1.2/move_test/Sketch.cpp
+++ 3D printer/Hall/HallTest_1.2/HallTest_1.2/move_test/Sketch.cpp
@@ -9,30 +9,33 @@
 
 
 
-#define motor_steps 200  //定義步進馬達每圈的步數  
+#define motor_steps 200																	//定義步進馬達每圈的步數
 #define step_divisition 16
 #define en_pin 5
 #define cw_pin 6
 #define clk_pin 7
+#define inPin 8
 
 StepperDriver ss(motor_steps, step_divisition, en_pin, cw_pin, clk_pin);  
 
-int main(){
+int StepsTaken = 0;
+
+void setup() { 
 	Serial.begin(9600);
-	Serial.print("Test Serial");
+	Serial.print("Serial Test\n");
+
+	pinMode(inPin, INPUT);        // sets the digital pin 7 as input
 
 	ss.setSpeed(160);
 	ss.powerEnable(true);
 	ss.positioning();
 	delay(1600);
-
-	while (1){
-		if (digitalRead(8)){
-
-		}
-		ss.setSpeed(160);
-		ss.step(20);
-		delay(100);
-	}
 }
 
+void loop() {
+	if (!digitalRead(8)){
+		StepsTaken += 1;
+		ss.step(1);
+	}
+	Serial.print(StepsTaken);
+}
