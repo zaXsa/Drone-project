7d4cc93c84645f25018783aab6c224470bd1cd7c
zaXsa <33417900+zaXsa@users.noreply.github.com>
2018-07-10 16:05:36 +0200

    Should work now

--- 3D printer/StepperMotor/StepperMotorTest_1.2/StepperMotorTest_1.2/Debug/StepperDriver.d
+++ 3D printer/StepperMotor/StepperMotorTest_1.2/StepperMotorTest_1.2/Debug/StepperDriver.d
@@ -1,10 +1,12 @@
-main.d main.o: .././main.cpp \
+StepperDriver.d StepperDriver.o: .././StepperDriver.cpp \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.132\include/avr/iom328p.h \
@@ -26,6 +28,10 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
---
+++ 3D printer/StepperMotor/StepperMotorTest_1.2/StepperMotorTest_1.2/StepperDriver.cpp
@@ -0,0 +1,145 @@
+﻿/******************************************************************************
+ * Project        : 3D printer
+ * File           : Sub program - Driver control
+ * Copyright      : 12018 Better Progress
+ ******************************************************************************
+  Change History:
+
+    Version 1.0 - July 12018		> Initial revision
+
+******************************************************************************/
+// ----------------------------------------------------------------------------
+// Libraries 
+// ----------------------------------------------------------------------------
+#define F_CPU 16000000UL
+
+#include <util/delay.h>
+#include <stdlib.h>
+#include <avr/io.h>
+
+#include "StepperDriver.h"
+
+// ----------------------------------------------------------------------------
+// Global variables
+// ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// Local function prototypes
+// ----------------------------------------------------------------------------
+void my_delay_ms(uint16_t n);
+
+/**
+  * @brief  The constructor sets the maximum amount of steps the devision factor of the driver and what pins are going to be used
+  * @param  Max steps the stepper can give, the devision factor of the driver and the used pin setup 
+  * @retval none
+  */
+StepperDriver::StepperDriver(int MaxSteps, int Devision, int StepsDistance,int Pins){
+	MaxNumberOfSteps = MaxSteps;
+	StepDevision = Devision;
+	UsedPins = Pins;
+	StepsPermm = StepsDistance;
+
+	StepInterval = 0;
+}
+
+/**
+  * @brief  This function makes the stepper take one step
+  * @param  none
+  * @retval none
+  */
+void StepperDriver::MoveStep(){
+	switch (UsedPins){
+		case 1:Driver1MoveON;my_delay_ms(StepInterval);Driver1MoveOFF;break;
+		case 2:Driver2MoveON;my_delay_ms(StepInterval);Driver2MoveOFF;break;
+		case 3:Driver3MoveON;my_delay_ms(StepInterval);Driver3MoveOFF;break;
+		case 4:Driver4MoveON;my_delay_ms(StepInterval);Driver4MoveOFF;break;
+		default:break;
+		}
+	
+}
+
+/**
+  * @brief  This function uses the wanted steps and the direction and makes sets the right configuration 
+  * @param  The amount of steps to take and the direction
+  * @retval none
+  */
+void StepperDriver::Steps(int16_t Stepstotake){
+	Stepstotake *= StepDevision;
+	setDirection(Stepstotake);
+
+	for (int16_t i = abs(Stepstotake); i > 0; i--) {
+		MoveStep();
+	}
+}
+
+/**
+  * @brief  This funtion makes sure the steppers are on the correct starting position before continuing   
+  * @param  none
+  * @retval none
+  */
+void StepperDriver::Positioning()
+{
+	uint16_t step_interval_0 = StepInterval;
+	StepInterval *= 8;
+
+	Steps(4);
+	_delay_ms(200);
+	Steps(-4);
+	_delay_ms(200);
+
+	StepInterval = step_interval_0;
+}
+
+/**
+  * @brief  This function sets the wanted revolutions per minute by changing the time interval between steps based on the steps it takes for one mm
+  * @param  Wanted revolutions per minute, the steps it takes per mm and the given step devision 
+  * @retval The new step interval time in ms 
+  */
+void StepperDriver::SetSpeed(const int WantedSpeed){
+  StepInterval = (WantedSpeed / (StepsPermm * StepDevision))*1000;
+
+  // StepInterval = 60000/(WantedSpeed*MaxNumberOfSteps);
+}
+/**
+  * @brief  This function enables or disables all driver 
+  * @param  The enable or disable command
+  * @retval none
+  */
+void StepperDriver::PowerEnable(const bool TrueFalse){
+	if (TrueFalse){
+		Driver1ON;
+	} else{
+		Driver1OFF;
+	}					
+}
+/**
+  * @brief  This function Checks the direction of the steps by checking if the given parameter is positive or negative 
+  * @param  The wanted steps
+  * @retval none
+  */
+void StepperDriver::setDirection(const int StepDirection){
+	if (StepDirection < 0) {
+		switch (UsedPins){
+		case 1:Driver1Left;break;
+		case 2:Driver2Left;break;
+		case 3:Driver3Left;break;
+		case 4:Driver4Left;break;
+		default:break;
+		}
+	}
+	else {
+		switch (UsedPins){
+		case 1:Driver1right;break;
+		case 2:Driver2right;break;
+		case 3:Driver3right;break;
+		case 4:Driver4right;break;
+		default:break;
+		}
+	}
+}
+
+void my_delay_ms(uint16_t n) {
+	for (uint16_t i=n;i>0;i--){
+		_delay_ms(1);
+	}
+}
