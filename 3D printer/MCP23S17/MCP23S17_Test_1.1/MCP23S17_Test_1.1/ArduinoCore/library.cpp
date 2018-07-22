5d8abfeb9e6172372125f377897fd2e3d11b70f3
zaXsa <33417900+zaXsa@users.noreply.github.com>
2018-07-22 13:25:43 +0200

    IO extended

---
+++ 3D printer/MCP23S17/MCP23S17_Test_1.1/MCP23S17_Test_1.1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 22-7-2018 13:22:39
+ * Author : renzo
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ 3D printer/MCP23S17/MCP23S17_Test_1.1/MCP23S17_Test_1.1/mcp23s17_word_example/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+MCP23S17.cpp
+
+Sketch.cpp
+
+src\libraries\SPI\SPI.cpp
+
